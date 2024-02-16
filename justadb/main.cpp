9e8c7117d06f9dcafb1fd33e903fc78a2cc08b77
Prabhat Sachdeva <prabhatexit0@gmail.com>
2024-02-17 03:30:47 +0530

    Remove everything from main

--- justadb/main.cpp
+++ justadb/main.cpp
@@ -1,12 +1,6 @@
+#include "dml.h"
 #include <optional>
 #include <string>
 #include <vector>
-#include "dml.h"
 
-
-int main() {
-  Query q(Query::Kind::SELECT, "table", {"column1", "column2"});
-  q.Where("column1", "value", WhereClause::Operator::EQUAL)
-      .OrderBy("column1", OrderByClause::Order::ASCENDING);
-  return 0;
-}
+int main() { return 0; }
