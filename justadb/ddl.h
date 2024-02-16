b3fabf82e00caeda761f3e13a3220f3efdb77d21
Prabhat Sachdeva <prabhatexit0@gmail.com>
2024-02-17 02:53:41 +0530

    Add types for DDL query

--- justadb/ddl.h
+++ justadb/ddl.h
@@ -62,3 +62,46 @@ private:
   std::vector<Column> columns_;
   std::vector<Tuple> tuples_;
 };
+
+class DdlQuery {
+public:
+  enum class Type { CREATE_TABLE, DROP_TABLE, ALTER_TABLE };
+  DdlQuery(Type type, std::string table_name)
+      : type_(type), table_name_(table_name) {}
+
+private:
+  Type type_;
+  std::string table_name_;
+};
+
+class CreateTableQuery : public DdlQuery {
+public:
+  CreateTableQuery(std::string table_name, std::vector<Column> columns)
+      : DdlQuery(Type::CREATE_TABLE, table_name), columns_(columns) {}
+
+  auto columns() const { return columns_; }
+
+private:
+  std::vector<Column> columns_;
+};
+
+class DropTableQuery : public DdlQuery {
+public:
+  DropTableQuery(std::string table_name)
+      : DdlQuery(Type::DROP_TABLE, table_name) {}
+};
+
+class AlterTableQuery : public DdlQuery {
+public:
+  enum class AlterType { ADD_COLUMN, DROP_COLUMN };
+  AlterTableQuery(std::string table_name, AlterType alter_type, Column column)
+      : DdlQuery(Type::ALTER_TABLE, table_name), alter_type_(alter_type),
+        column_(column) {}
+
+  auto alter_type() const { return alter_type_; }
+  auto column() const { return column_; }
+
+private:
+  AlterType alter_type_;
+  Column column_;
+};
