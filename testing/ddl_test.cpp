d1dce2fdeae22f914b249756d7a44c51bf8c7af6
Prabhat Sachdeva <prabhatexit0@gmail.com>
2024-03-03 11:33:09 +0530

    Add checks for query exec result in ddl test

--- testing/ddl_test.cpp
+++ testing/ddl_test.cpp
@@ -12,13 +12,13 @@ TEST(DDLTest, DdlQueriesTest) {
 
   DdlQueryExec ddl_query_exec(&db_manager);
   auto query_exec_result = ddl_query_exec.ExecuteCreateDatabaseQuery(create_db_query);
-  EXPECT_TRUE(query_exec_result);
+  EXPECT_TRUE(query_exec_result) << "Error while executing Create DB Query";
   EXPECT_TRUE(db_manager.GetDatabase(DB_NAME)) << "Database not created";
 
   // User Database Query test
   UseDatabaseQuery use_db_query(DB_NAME);
   query_exec_result = ddl_query_exec.ExecuteUseDatabaseQuery(use_db_query);
-  EXPECT_TRUE(query_exec_result);
+  EXPECT_TRUE(query_exec_result) << "Error while executing use database query";
   EXPECT_TRUE(db_manager.current_database()) << "Current database not set";
   EXPECT_EQ(DB_NAME, db_manager.current_database().value()->name())
       << "Current database name mismatch";
@@ -29,9 +29,10 @@ TEST(DDLTest, DdlQueriesTest) {
   // Create Table Query test
   auto *column_1 = new Column(COLUMN_NAME, Column::Type::INT);
   CreateTableQuery create_table_query(TABLE_NAME, {column_1});
-  const auto table_from_exec_query =
+  const auto create_table_exec_result =
       ddl_query_exec.ExecuteCreateTableQuery(create_table_query);
-  EXPECT_TRUE(table_from_exec_query.has_value())
+  EXPECT_TRUE(query_exec_result) << "Error while executing create table query";
+  EXPECT_TRUE(create_table_exec_result.has_value())
       << "Table not returned from executing create table query";
 
   const auto table_from_db_access =
@@ -52,6 +53,4 @@ TEST(DDLTest, DdlQueriesTest) {
   auto *column_2 = new Column(COLUMN_NAME_0, Column::Type::INT);
   AlterTableQuery add_column_query(
       TABLE_NAME, JustADb::AlterTableQuery::AlterType::ADD_COLUMN, column_2);
-
-  delete column_1;
 }
